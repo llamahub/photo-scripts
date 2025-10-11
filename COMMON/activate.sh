de470fb8c338c04178587045a62cb205e820268d
incatrex <incatrex@gmail.com>
2025-10-11 00:36:30 -0400

    fix issue with linux gtest

--- COMMON/activate.sh
+++ COMMON/activate.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-source "/workspaces/photo-scripts/COMMON/.venv/bin/activate"
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/.venv/bin/activate"
